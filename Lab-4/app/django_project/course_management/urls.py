e8d667c0f57b74bef8089cf34fa79220b7fbb80d
t3-b0 <estebannicolas.palma@userena.cl>
2023-10-20 12:25:30 -0300

    Se modifican "urls.py" para redireccionar a movies.html, y "views.py" para la lógica de conexión a la API de TMDB y su posterior recopilación de información por movies.html. Además, se crea "movies.html" para mostrar las películas en cartelera.

--- Lab-4/app/django_project/course_management/urls.py
+++ Lab-4/app/django_project/course_management/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.get_now_playing_movies, name="now_playing_movies"),
+    path('movies/', views.get_now_playing_movies, name='now_playing_movies')
 ]
--- Lab-4/app/django_project/course_management/views.py
+++ Lab-4/app/django_project/course_management/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
 from .models import Course, Student
+import requests
+
 
 def index(request):
     courses = Course.objects.all()
     return render(request, 'index.html',{'courses':courses})
+
+
+def get_now_playing_movies(request):
+    # URL de la API de TheMovieDB
+    url = "https://api.themoviedb.org/3/movie/now_playing"
+
+    # Parámetros de la solicitud, incluyendo la clave de la API
+    params = {
+        "api_key": "d6bbd6fbd712b54e3f8a7562489a33ac"  # Reemplaza con tu propia API Key
+    }
+
+    try:
+        # Realizar la solicitud GET a la API
+        response = requests.get(url, params=params)
+
+        # Verificar si la solicitud fue exitosa (código de respuesta 200)
+        if response.status_code == 200:
+            # Parsear la respuesta JSON
+            data = response.json()
+            movies = data['results']
+            return render(request, 'movies.html', {'movies': movies})
+        else:
+            # Manejo de errores si la solicitud no fue exitosa
+            return render(request, 'error.html', {'error_message': 'No se pudo obtener la información de las películas'})
+    except Exception as e:
+        # Manejo de excepciones
+        return render(request, 'error.html', {'error_message': str(e)})
