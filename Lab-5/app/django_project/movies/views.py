417a4e76c4c4164420454dab8f6150c8e18f7ca8
t3-b0 <estebannicolas.palma@userena.cl>
2023-11-24 23:38:21 -0300

    Se agrega jquery 3.7.1 (no estaba en los static). Se agrega un formulario que utiliza llamadas al a API de Filmon VOD para rescatar películas con los tags Nombre, Año, Descripción, Imagen y Detalles, utilizando AJAX para refrescar de forma dinámica las categorías.

--- Lab-5/app/django_project/movies/views.py
+++ Lab-5/app/django_project/movies/views.py
@@ -1,7 +1,17 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 import requests
 
 def index(request):
-    
-
     return render(request, 'index.html')
+
+def get_genres(request):
+    response = requests.get('http://api.filmon.com/api/vod/genres')
+    genres = response.json()
+    return JsonResponse(genres)
+
+def get_movies(request):
+    genre_id = request.GET.get('genre')
+    response = requests.get(f'http://api.filmon.com/api/vod/search?genre={genre_id}')
+    movies = response.json()
+    return JsonResponse(movies)
